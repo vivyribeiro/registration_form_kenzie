d70a0089e218251ba677606dcad545e3b40354e6
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2022-06-09 01:00:44 -0300

    Fixing JS Code

--- src/script.js
+++ src/script.js
@@ -1,38 +1,10 @@
 /* Register Confirmation */
+let participantData = [];
 
 const form = document.getElementById("form");
 form.addEventListener("submit", e => {
 	e.preventDefault();
 
-	let content = document.getElementById("form__message");
-	let registering = `<span>Cadastrando...</span>`;
-	let confirmation = `<span>Cadastrado com sucesso!</span>`;
-
-	cleanField();
-
-	content.innerHTML = registering;
-	setTimeout(() => {
-		content.innerHTML = confirmation;
-	}, 1000);
-});
-
-/* Cleaning All Fields from Form */
-
-function cleanField() {
-	document.querySelector("#name").value = "";
-	document.querySelector("#email").value = "";
-	document.querySelector("#description").value = "";
-	document.querySelector("#modules__kenzie").value = "";
-	document.querySelector("#password").value = "";
-	document.querySelector("#confirm__password").value = "";
-	document.querySelector(
-		'input[name = "radio__options"]:checked'
-	).checked = false;
-}
-
-/* Saving Data from Form to the localStorage */
-
-function saveDataParticipant() {
 	//  Getting Data from Form
 	const name = document.getElementById("name").value;
 	const email = document.getElementById("email").value;
@@ -44,25 +16,8 @@ function saveDataParticipant() {
 		'input[name = "radio__options"]:checked'
 	).value;
 
-	// Showing Ddata on Console
-	console.log(
-		name +
-			"\n" +
-			email +
-			"\n" +
-			descriptionBox +
-			"\n" +
-			selectModule +
-			"\n" +
-			password +
-			"\n" +
-			confirmPassword +
-			"\n" +
-			sendNewsletter
-	);
-
 	// Grouping Data in an Object
-	const participantData = {
+	participantData.push({
 		name,
 		email,
 		descriptionBox,
@@ -70,9 +25,62 @@ function saveDataParticipant() {
 		password,
 		confirmPassword,
 		sendNewsletter
-	};
+	});
 
 	// Storing Data on LocalStorage
 	let convertData = JSON.stringify(participantData);
 	localStorage.setItem("participant", convertData);
-}
+
+	/* Content Message*/
+	let content = document.getElementById("form__message");
+	let confirmation = `<span>Cadastrado com sucesso!</span>`;
+	let clearMessage = "";
+
+	/* Cleaning All Fields from Form */
+	form.reset();
+
+	/* Confirmation Message*/
+	content.innerHTML = confirmation;
+	setTimeout(() => {
+		content.innerHTML = clearMessage;
+	}, 3000);
+
+	/* Checked Default Option */
+	document.querySelector(
+		'input[name = "radio__options"]:checked'
+	).checked = true;
+});
+
+// function cleanField() {
+// 	form.reset();
+// document.querySelector("#name").value = "";
+// document.querySelector("#email").value = "";
+// document.querySelector("#description").value = "";
+// document.querySelector("#modules__kenzie").value = "";
+// document.querySelector("#password").value = "";
+// document.querySelector("#confirm__password").value = "";
+// document.querySelector(
+// 	'input[name = "radio__options"]:checked'
+// ).checked = false;
+// }
+
+/* Saving Data from Form to the localStorage */
+
+// function saveDataParticipant() {
+// 	// Showing Ddata on Console
+// 	console.log(
+// 		name +
+// 			"\n" +
+// 			email +
+// 			"\n" +
+// 			descriptionBox +
+// 			"\n" +
+// 			selectModule +
+// 			"\n" +
+// 			password +
+// 			"\n" +
+// 			confirmPassword +
+// 			"\n" +
+// 			sendNewsletter
+// 	);
+// }
